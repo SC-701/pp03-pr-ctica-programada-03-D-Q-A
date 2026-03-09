b92b2519d4ec1e405c6b4db300605dfe9665d72e
Funnyd <dquesadaa2004@gmail.com>
2026-03-09 17:19:01 -0600

    Practica Completa

---
+++ Vehiculo.API/API/Controllers/VehiculoController.cs
@@ -0,0 +1,79 @@
+﻿using Abstracciones.Interfaces.API;
+using Abstracciones.Interfaces.Flujo;
+using Abstracciones.Modelos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VehiculoController : ControllerBase, IVehiculoController
+    {
+
+        private IVehiculoFlujo _vehiculoFlujo;
+        private ILogger<VehiculoController> _vehiculoLogger;
+
+       
+
+        #region "Operaciones"
+        public VehiculoController(IVehiculoFlujo vehiculoFlujo, ILogger<VehiculoController> logger)
+        {
+            _vehiculoFlujo = vehiculoFlujo;
+            _vehiculoLogger = logger;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Agregar([FromBody] VehiculoRequest vehiculo)
+        {
+            var resultado = await _vehiculoFlujo.Agregar(vehiculo);
+            return CreatedAtAction(nameof(Obtener), new { Id = resultado }, null);
+        }
+
+        [HttpPut("{Id}")]
+        public async Task<IActionResult> Editar([FromRoute] Guid Id, [FromBody] VehiculoRequest vehiculo)
+        {
+            if (!await VerificarExistencia(Id))
+                return NotFound("Vehiculo no existe");
+            var resultado = await _vehiculoFlujo.Editar(Id, vehiculo);
+            return Ok(resultado);
+        }
+
+
+        [HttpDelete("{Id}")]
+        public async Task<IActionResult> Eliminar([FromRoute] Guid Id)
+        {
+            if (!await VerificarExistencia(Id))
+                return NotFound("Vehiculo no existe");
+            var resultado = await _vehiculoFlujo.Eliminar(Id);
+            return NoContent();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Obtener()
+        {
+            var resultado = await _vehiculoFlujo.Obtener();
+            if(!resultado.Any())
+                return NoContent();
+            return Ok(resultado);
+        }
+
+        [HttpGet("{Id}")]
+        public async Task<IActionResult> Obtener([FromRoute] Guid Id)
+        {
+            var resultado = await _vehiculoFlujo.Obtener(Id);
+            return Ok(resultado);
+        }
+        #endregion
+
+        #region "Helpers"
+        private async Task<bool> VerificarExistencia(Guid Id)
+        {
+            var Validacion = false;
+            var Existe = await _vehiculoFlujo.Obtener(Id);
+            if (Existe != null)
+                return Validacion = true;
+            return Validacion;
+        }
+        #endregion
+    }
+}
